cd7cf862acf7b480d56376260830b88986dd5887
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-08-13 09:31:51 +0430

    product update applied to for doller goods

--- src/store/filters.py
+++ src/store/filters.py
@@ -10,3 +10,39 @@ class ProductsFilter(django_filters.FilterSet):
     class Meta:
         model = Products
         fields = ["category"]
+
+
+# filters.py
+import django_filters
+import jdatetime
+from datetime import datetime, timedelta
+from .models import SalesDetails
+
+class SalesDetailsFilter(django_filters.FilterSet):
+    from_date = django_filters.CharFilter(method='filter_from_date')
+    to_date = django_filters.CharFilter(method='filter_to_date')
+
+    class Meta:
+        model = SalesDetails
+        fields = []
+
+    def _convert_jalali_to_gregorian(self, jalali_str):
+        try:
+            year, month, day = map(int, jalali_str.split('-'))
+            jalali_date = jdatetime.date(year, month, day)
+            gregorian = jalali_date.togregorian()
+            return datetime(gregorian.year, gregorian.month, gregorian.day)
+        except Exception:
+            return None
+
+    def filter_from_date(self, queryset, name, value):
+        date = self._convert_jalali_to_gregorian(value)
+        if date:
+            return queryset.filter(created_at__gte=date)
+        return queryset
+
+    def filter_to_date(self, queryset, name, value):
+        date = self._convert_jalali_to_gregorian(value)
+        if date:
+            return queryset.filter(created_at__lt=(date + timedelta(days=1)))
+        return queryset
